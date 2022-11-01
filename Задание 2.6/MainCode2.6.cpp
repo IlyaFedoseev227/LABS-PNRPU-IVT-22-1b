17d32001b0ebbeece1489ce26982b107dcfe8fa5
Илья Федосеев <olegf800@gmail.com>
2022-11-01 19:41:51 +0300

    Коммичу норм задания

---
+++ Задание 2.6/MainCode2.6.cpp
@@ -0,0 +1,57 @@
+﻿
+//Задача 2.6
+//
+//Условие: напечатать заполненный прямоугольный равнобедренный треугольник из `*` со сторонами катетов, 
+//равными N, и прямым углом справа сверху.где N — натуральное число больше 2.
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    start:
+    float n;
+    char zvezda = '*';
+    char probel = ' ';
+    int colvoProbelov;
+    int colvoZvezd = 1;
+
+    std::cout << "Введите длину катетов N:" << std::endl;
+    std::cin >> n;
+
+    if ((n <= 2) or ((int)n != n))
+    {
+        std::cout << "Неверно!" << std::endl;
+        goto start; // на строку 13
+    }
+
+    colvoProbelov = 0;
+    colvoZvezd = n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= colvoProbelov; j++)
+        {
+            std::cout << probel;
+        }
+
+        for (int j = 1; j <= colvoZvezd; j++)
+        {
+            std::cout << zvezda;
+        }
+
+        colvoProbelov++;
+        colvoZvezd--;
+
+        if (colvoZvezd < n)
+        {
+            std::cout << std::endl;
+        }
+
+        if (colvoZvezd == n)
+        {
+            break;
+        }
+    }
+}
---
+++ Задание 2.7/MainCode2.7.cpp
@@ -0,0 +1,92 @@
+﻿
+//Задача 2.7
+//
+//Условие: Напечатать квадрат из N звездочек.Реализовать все три варианта.
+//
+//А) В котором ВСЕГО N звездочек.
+//Б) Заполненный квадрат со стороной N.
+//В) Пустой квадрат со стороной N. В каждом из вариантов задать требования к минимальному N.
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    
+    start:
+    float n;
+    int vsegoZvezd;
+    int vibor;
+    char zvezda = '*';
+    char probel = ' ';
+
+    std::cout << "Введите число N:" << std::endl;
+    std::cin >> n;
+
+    if ((n <= 0) or ((int)n != n))
+    {
+        std::cout << "Неверно!" << std::endl;
+        goto start; // на строку 16
+    }
+
+    std::cout << "Что тебе нужно?\n" 
+        "1) Квадрат в котором ВСЕГО N звездочек.(если введено нечетное N, то округление будет в меньшую сторону\n"
+        "2) Заполненный квадрат со стороной N.\n"
+        "3) Пустой квадрат со стороной N." << std::endl;
+    std::cin >> vibor;
+    
+    if ((vibor < 1) and (vibor > 3))
+    {
+        std::cout << "Неверно!" << std::endl;
+        goto start; // на строку 16
+    }
+
+    switch (vibor)
+    {
+        case 1: // НЕ РАБОТАЕТ!
+            vsegoZvezd = n / 3;
+            for (int i = 0; i < vsegoZvezd; i++)
+            {
+                for (int j = 0; j < vsegoZvezd; j++)
+                {
+                    std::cout << zvezda;
+                }
+                std::cout << std::endl;
+            }
+            break;
+        case 2:
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    std::cout << zvezda;
+                }
+                std::cout << std::endl;
+            }
+            break;
+        case 3:
+            for (int i = 0; i < n; i++)
+            {
+                std::cout << zvezda;
+            }
+            std::cout << std::endl;
+            for (int i = 0; i < n - 2; i++)
+            {
+                std::cout << zvezda;
+                for (int j = 0; j < n - 2; j++)
+                {
+                    std::cout << probel;
+                }
+                std::cout << zvezda;
+                std::cout << std::endl;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                std::cout << zvezda;
+            }
+            std::cout << std::endl;
+            break;
+        default:
+            break;
+    }
+}
