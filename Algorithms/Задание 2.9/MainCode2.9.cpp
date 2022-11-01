adba036c8d3b9013efe2cd4a8cd0f2f8dbf396ca
Илья Федосеев <olegf800@gmail.com>
2022-11-02 00:04:03 +0300

    Home

---
+++ Algorithms/Задание 2.9/MainCode2.9.cpp
@@ -0,0 +1,33 @@
+﻿
+//Задача 2.9
+//
+//Условие: Построить пустой куб из звездочек.
+
+#include <iostream>
+
+int main()
+{
+    int colvoZvezd = 10;
+
+	for (int i = 0; i < colvoZvezd; i++)
+	{
+		std::cout << '*';
+	}
+	std::cout << std::endl;
+
+	for (int i = 0; i < colvoZvezd - 2; i++)
+	{
+		std::cout << '*';
+		for (int j = 0; j < colvoZvezd - 2; j++)
+		{
+			std::cout << ' ';
+		}
+		std::cout << '*' << std::endl;
+	}
+
+	for (int i = 0; i < colvoZvezd; i++)
+	{
+		std::cout << '*';
+	}
+	std::cout << std::endl;
+}
