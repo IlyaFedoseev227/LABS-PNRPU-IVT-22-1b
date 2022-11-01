03970f723e6d479d860b53587c092156b0e98d05
Илья Федосеев <olegf800@gmail.com>
2022-11-01 18:41:02 +0300

    Home
    Home

---
+++ Домаха от 26.10.22/ConsoleApplication1/MainCode2.3.cpp
@@ -0,0 +1,46 @@
+﻿
+//Задача 2.3
+//
+//Условие: посчитать сумму `S = 1 * 2 + 2 * 3 * 4 + 3 * 4 * 5 * 6 + 4 * 5 * 6 * 7 * 8 + ....` 
+//до N(натурального числа).
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    start:
+    int s = 0;
+    int sum;
+    int flag = 1;
+    int z = 1;
+    float n;
+    
+    std::cout << "Введите число N (целое, больше и не равно нулю):" << std::endl;
+    std::cin >> n;
+
+    if ((n <= 0) or (int(n) != n))
+    {
+        std::cout << "Неверно!" << std::endl;
+        goto start; // на строку 13
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum = i;
+        for (int j = flag; j <= flag + z; j++)
+        {
+            sum *= j;
+        }
+        flag = 2;
+        s += sum;
+        flag += i;
+        if (flag >= 4)
+        {
+            z += 1;
+        }
+    }
+
+    std::cout << s << std::endl;
+}
---
+++ Домаха от 26.10.22/Задание 2.5/MainCode2.5.cpp
@@ -0,0 +1,55 @@
+﻿
+//Задача 2.5
+//
+//Условие: напечатать заполненный прямоугольный равнобедренный треугольник из `*` со сторонами катетов, 
+//равными N, и прямым углом справа снизу.где N — натуральное число больше 2.
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    start:
+    float n;
+    char zvezda = '*';
+    char probel = ' ';
+    int colvoProbelov;
+    int colvoZvezd = 1;
+
+    std::cout << "Введите длину катетов N:" << std::endl;
+    std::cin >> n;
+
+    if ((n <= 2) or ((int)n != n))
+    {
+        std::cout << "Неверно!" << std::endl;
+        goto start; // на строку 13
+    }
+
+    colvoProbelov = n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        colvoProbelov--;
+        colvoZvezd = i;
+        for (int j = 1; j <= colvoProbelov; j++)
+        {
+            std::cout << probel;
+        }
+
+        for (int j = 1; j <= colvoZvezd; j++)
+        {
+            std::cout << zvezda;
+        }
+
+        if (colvoZvezd < n)
+        {
+            std::cout << std::endl;
+        }
+
+        if (colvoZvezd == n)
+        {
+            break;
+        }
+    }
+}
