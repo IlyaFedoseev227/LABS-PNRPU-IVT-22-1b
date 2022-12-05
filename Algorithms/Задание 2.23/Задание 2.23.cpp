6c0166b23bfac5741edb6271f618274cc4a788c6
Илья Федосеев <olegf800@gmail.com>
2022-12-05 22:21:38 +0300

    Финал

--- Algorithms/Задание 2.23/Задание 2.23.cpp
+++ Algorithms/Задание 2.23/Задание 2.23.cpp
@@ -3,35 +3,37 @@
 //Последовательность элементов задана формулой общего члена <img src = "https://latex.codecogs.com/svg.latex?%5Cinline%20a_%7Bi%7D%20%3D%20sin%28n%20&plus;%20%5Cfrac%7Bi%7D%7Bn%7D%29">
 
 #include <iostream>
+#include <cmath>
 
 int main()
 {
-    setlocale(LC_ALL, "Rus"); // ЗАДАЧА В ПРОЦЕССЕ
+    setlocale(LC_ALL, "Rus");
 
-    float x, n, dlina, max, count = 0, stopka;
+    float n, a, max;
+    int i = 2;
+    int count = 1;
 
-    std::cout << "Введите число n:" << std::endl;
+    std::cout << "Введите n: " << std::endl;
     std::cin >> n;
-    std::cout << "Введите длину последовательности:" << std::endl;
-    std::cin >> dlina;
 
-    x = sin(n + 1 / n);
-    x = round(x * 1000) / 1000;
-    max = x;
-    
-    if (max == )
+    max = sin(n + 1 / n);
+
+    while (i <= n)
     {
-
-    }
-
-    for (int i = 2; i <= dlina; i++)
-    {
-        x = sin(n + i / n);
-        x = round(x * 1000) / 1000;
-
-        if (x > max)
+        a = sin(n + 1 / n);
+        if (a > max)
         {
-            max = x;
+            max = a;
+            count = 1;
         }
+        else if (a == max)
+        {
+            count++;
+        }
+        i++;
     }
+
+    std::cout << "Максимальный элемент: " << max << " Его номер: " << count << std::endl;
+    std::cout << "Максимальный элемент: " << max << std::endl;
+    std::cout << "Число элементов с этим значением: " << count << std:: endl;
 }
