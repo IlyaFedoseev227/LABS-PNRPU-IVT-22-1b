e623f070194140ffd2a4ea02fc264785076832d0
Илья Федосеев <olegf800@gmail.com>
2023-02-08 13:34:13 +0300

    sort

---
+++ Labs/Сортировка вставкой/Сортировка вставкой.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+int main()
+{
+	const int SIZE = 10;
+	int array[SIZE] = { 10, 43, 22, -10, 5, 32, 54, -32, 55, 16 };
+
+	for (int i = 1; i < SIZE; i++) {
+		for (int j = i; (j > 0) and (array[j] < array[j - 1]); j--) {
+			std::swap(array[j - 1], array[j]);
+		}
+	}
+
+	for (int i = 0; i < SIZE; i++) {
+		std::cout << array[i] << std::endl;
+	}
+}
