055f826a506ae2cb675663971743a48f072edc7f
Илья Федосеев <olegf800@gmail.com>
2023-02-27 03:37:13 +0300

    Create 7.2 Лаба.cpp

---
+++ 2 Семестр/Labs/7.2 Лаба/7.2 Лаба.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <stdarg.h>
+float mult(int k, ...){
+	float s = 1;
+	va_list fack;
+	va_start(fack, k);
+	for (int i = 0; i < k; i++){
+	     float num = va_arg(fack, double);
+		 s *= num;
+	}
+	va_end(fack);
+	return s;
+}
+int main(){
+	std::cout << "\nSum(2, 4, 6) = " << mult(3, 44.5f, 2.2f, 2.3f);
+	std::cout << "\nSum(4, 1, 2, 3, 4) = " << mult(7, 1.1f, 2.0f, 3.0f, 4.0f, 4.0f, 4.5f, 6.6f);
+	std::cout << "\nSum(4, 1, 2, 3, 4) = " << mult(11, 1.2f, 2.0f, 3.0f, 4.0f, 4.3f, 4.5f, 2.6f, 3.3f, 2.1f, 2.5f, 2.3f);
+}
