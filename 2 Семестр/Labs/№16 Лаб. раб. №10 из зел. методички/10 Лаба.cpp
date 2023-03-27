375dcb1824ecaf2446641385c6cfa23428365233
Илья Федосеев <olegf800@gmail.com>
2023-03-27 14:15:39 +0300

    Create 10 Лаба.cpp

---
+++ 2 Семестр/Labs/№16 Лаб. раб. №10 из зел. методички/10 Лаба.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+int main() {
+    srand(time(NULL));
+    int n, k;
+
+    std::cout << "Enter the length of the array: " << std::endl;
+    std::cin >> n;
+
+    int* arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 20;
+    }
+
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Enter the key: " << std::endl;
+    std::cin >> k;
+
+    for (int i = k - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+
+    delete[] arr;
+    return 0;
+}
