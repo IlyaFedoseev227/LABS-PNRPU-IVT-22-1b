cee7d057c31af79f5163c75d6a25589a80fb94b6
Илья Федосеев <olegf800@gmail.com>
2023-03-29 01:22:45 +0300

    Create Шелла.cpp

---
+++ 2 Семестр/Labs/№9 Сложные сортировки быстрая (Хоара), слиянием, подсчетом, блочная, Шелла/Шелла.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main() {
+	srand(time(NULL));
+	int n, d, j, tmp;
+	std::cout << "Type" << std::endl;
+	std::cin >> n;
+	int* a = new int[n];
+	std::cout << "Your array is:";
+	for (int i = 0; i < n; i++) {
+		a[i] = rand() % 101;
+		cout << a[i] << ' ';
+	}
+	std::cout << std::endl;
+	d = n / 2;
+	while (d > 0) {
+		for (int i = 0; i < n - d; i++) {
+			j = i;
+			while (j >= 0 and a[j] > a[j + d]) {
+				tmp = a[j];
+				a[j] = a[j + d];
+				a[j + d] = tmp;
+				j -= d;
+			}
+		}
+		d = d / 2;
+	}
+	std::cout << "Sorted:";
+	for (int i = 0; i < n; i++)
+		std::cout << a[i] << ' ';
+	delete[] a;
+	return 0;
+}
