13d17688cf7a810263f00aed21fc99f757bdc51d
Илья Федосеев <olegf800@gmail.com>
2023-05-22 01:05:43 +0300

    к

---
+++ 2 Семестр/Class/Лаба 8/Dialog.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "list.h"
+#include "Event.h"
+
+class Dialog :public List {
+protected:
+    int EndState;
+
+public:
+    Dialog();
+    ~Dialog() {};
+    virtual void GetEvent(TEvent& event);
+    virtual void HandleEvent(TEvent& event);
+    virtual void ClearEvent(TEvent& event);
+    virtual int Execute();
+    int Valid();
+    void EndExec();
+};
